ecb4e0f5b1e697ffeb11a4d08a7fcd08e04f0817
ayto <ayto_7@163.com>
2018-05-08 15:06:19 +0800

    定时器

--- xiajin/js/main.js
+++ xiajin/js/main.js
@@ -1,9 +1,9 @@
-$(function(){
-
-    (function(){
+$(function () {
+    
+    (function () {
         var src = $("#loading > img").attr("src");
         var img = new Image();
-        img.onload = function(){
+        img.onload = function () {
             console.log("OKOKOK");
             loadFn({
                 loadImg: {
@@ -36,121 +36,143 @@ $(function(){
                     i27: "img/prevP.png",
                     i28: "img/qr.png"
                 },
-                complete:init
+                complete: init
             });
         }
         img.src = src;
     })();
-
+    
     document.addEventListener("WeixinJSBridgeReady", function () {
-
+        
         $('.bgm')[0].play();
     }, false);
-    document.addEventListener('YixinJSBridgeReady', function() {
+    
+    document.addEventListener('onWeixinJSBridgeReady', function () {
+    
+        $('.bgm')[0].play();
+    }, false);
+    
+    
+    document.addEventListener('YixinJSBridgeReady', function () {
+        alert(12)
         $('.bgm')[0].play();
     }, false);
     $('.bgm')[0].play();
-    $(document).on("click", function(){
+    $(document).on("click", function () {
         alert($('.bgm').attr('src'))
         $('.bgm')[0].play();
-    })
-    $('.music').on('touchstart',function(){
-        if($(this).hasClass('okPlay')){
+    });
+    $('.music').on('click', function () {
+        if ($(this).hasClass('okPlay')) {
             $('.bgm')[0].pause();
-            $(this).removeClass('okPlay').attr("src","http://sd-gold.oss-cn-beijing.aliyuncs.com/web/mmm1.png").removeClass("musicAnima");
-        }else{
+            $(this).removeClass('okPlay').attr("src", "http://sd-gold.oss-cn-beijing.aliyuncs.com/web/mmm1.png").removeClass("musicAnima");
+        } else {
             $('.bgm')[0].play();
-            $(this).addClass('okPlay').attr("src","http://sd-gold.oss-cn-beijing.aliyuncs.com/web/mmm.png").addClass("musicAnima");
+            $(this).addClass('okPlay').attr("src", "http://sd-gold.oss-cn-beijing.aliyuncs.com/web/mmm.png").addClass("musicAnima");
         }
     });
-
-
-
+    
+    
     /* 添加动画名 */
     var $item = $(".item");
-    $item.find(".page3-1").addClass("animated").data("a","fadeInLeft");
-    $item.find(".title").addClass("animated").data("a","puffIn1");
-    $(".item .txt p").addClass("animated").data("a","fadeIn");
-    $(".back-menu").addClass("animated").data("a","fadeInLeft");
-    $(".page-mark").addClass("animated").data("a","fadeInRight");
-
-    function init(imgs){
-
-        setTimeout(function(){
-
+    $item.find(".page3-1").addClass("animated").data("a", "fadeInLeft");
+    $item.find(".title").addClass("animated").data("a", "puffIn1");
+    $(".item .txt p").addClass("animated").data("a", "fadeIn");
+    $(".back-menu").addClass("animated").data("a", "fadeInLeft");
+    $(".page-mark").addClass("animated").data("a", "fadeInRight");
+    
+    function init(imgs) {
+        
+        setTimeout(function () {
+            
             $('#loading').hide();
             $(".prevP").show();
             $(".music").show();
-            for(var i = 0; i < $(".animated").length; i++){
-                $(".animated").eq(i).addClass($(".animated").eq(i).data("a")).css("opacity","1");
+            for (var i = 0; i < $(".animated").length; i++) {
+                $(".animated").eq(i).addClass($(".animated").eq(i).data("a")).css("opacity", "1");
             }
-
-        },1000);
+            
+        }, 1000);
         /* TODO 修改上方时间 ↑↑*/
-
-
-
+        
+        
     }
+    
     //清楚默认事件
-    $(document).on("touchstart",function(e){
+    $(document).on("touchstart", function (e) {
         e.stopPropagation();
     });
-
-
+    
+    
     var num = $(".swiper-slide").size();
-
+    
     /* siwper实例化 */
     var mySwiper = new Swiper('.swiper-container', {
         initialSlide: 0,
-        direction : 'vertical',
-        onSlideChangeEnd: function(swiper) {//划完
+        direction: 'vertical',
+        onSlideChangeEnd: function (swiper) {//划完
             var index = swiper.activeIndex;
             var ad = $(".swiper-slide").eq(index).find(".animated");
             for (var i = 0; i < ad.length; i++) {
                 ad.eq(i).addClass(ad.eq(i).data("a")).css("opacity", "1");
             }
-
-            if(index = num){
+            
+            if (index = num) {
                 $(".prevP").hide();
             }
         },
-        onSlideChangeStart: function(swiper){//刚开始划
-            for(var i = 0; i < $(".animated").length; i++){
-                $(".animated").eq(i).removeClass($(".animated").eq(i).data("a")).css("opacity","0");
+        onSlideChangeStart: function (swiper) {//刚开始划
+            for (var i = 0; i < $(".animated").length; i++) {
+                $(".animated").eq(i).removeClass($(".animated").eq(i).data("a")).css("opacity", "0");
             }
         }
     });
-
-
+    
+    
     /* 目录切换 */
-    $("#page2 .tab").on("click", function(){
-
+    $("#page2 .tab").on("click", function () {
+        
         var index = $(this).data("index");
         $(this).siblings(".tab").removeClass("active");
         $(this).addClass("active");
         $("#page2 .box").eq(index - 1).show().siblings(".box").hide();
     });
-
-
+    
+    
     /* 目录点击 */
-    (function(){
-
+    (function () {
+        
         var node = $("#page2 .box li");
-
-        for(var i = 0; i < node.length; i++){
+        
+        for (var i = 0; i < node.length; i++) {
             node.eq(i).attr("name", i);
-            node.eq(i).on("click", function(){
+            node.eq(i).on("click", function () {
                 var index = parseInt($(this).attr("name"));
                 mySwiper.slideTo(index + 2);
             });
-
-
+            
+            
         }
     })();
-
+    
     /* 返回目录 */
-    $(".back-menu").on("click", function(){
+    $(".back-menu").on("click", function () {
         mySwiper.slideTo(1);
     });
-
+    
+    
+    /*
+    * 我想说的是事件绑定的问题
+    * 比如click事件
+    * */
+    $(tag).click(function(){})
+    // 和↓ 是一样的 只能绑定一次。
+    tag.onclick = function(){}
+    
+    // 而
+    $(tag).on("click", function(){})
+    // 是和↓ 是一样的 可以重复绑定。
+    tag.addEventListener("click", function(){})
+    
+    
 });
